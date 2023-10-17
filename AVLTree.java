7bb74e857c311e2ca768984b0543010ac0443dc3
dex-devries <dexter.m.devries@gmail.com>
2023-10-17 11:35:36 -0700

    First commit and push

---
+++ AVLTree.java
@@ -0,0 +1,13 @@
+/*
+ * Authors: Dexter DeVries, 
+ * Description: An implementation of a self-balancing binary search tree (AVL tree). 
+ * 
+ */
+
+public class AVLTree {
+
+    // constructor
+    public AVLTree() {
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+/*
+ * Used for testing the AVLTree
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        // AVLTree tree = new AVLTree();
+    }
+}

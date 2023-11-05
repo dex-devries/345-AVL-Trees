6f43ebf9d65b69246823ba0fbc81bedc213f1335
anishamunjal <anishamunjal@arizona.edu>
2023-11-04 19:10:12 -0700

    Change for fixing height

--- AVLTree.java
+++ AVLTree.java
@@ -10,267 +10,254 @@
  */
 
 public class AVLTree {
-    // class Node - a node in a BST
-    class Node {      
-        int val;  // the integer value for this node
-        int height;  // the height of this node in the tree
-        Node left;  // the left child node - inits to null
-        Node right;  // the right child node - inits to null
-      
-        // constructor with int val as parameter - creates a Node with value val
-        public Node(int val) {  
-            left = null;  
-            right = null;  
-            this.val = val;  
-            height = 0;  
-        }       
-    }  
+	// class Node - a node in a BST
+	class Node {
+		int val; // the integer value for this node
+		int height; // the height of this node in the tree
+		Node left; // the left child node - inits to null
+		Node right; // the right child node - inits to null
 
-    private int nodeCount;	// number of nodes in AVL tree
-	public Node root;   // the root of the tree
-    private int height; // the height of the tree
+		// constructor with int val as parameter - creates a Node with value val
+		public Node(int val) {
+			left = null;
+			right = null;
+			this.val = val;
+			height = 0;
+		}
+	}
 
-    // constructor - initialize fields
-    public AVLTree() {
-        this.nodeCount = 0;
-        this.root = null;
-        this.height = 0;
-    }
-    
-    /**
-     * Returns true when AVL tree is empty, false otherwise
-     * 
-     * @return boolean true, if empty, false otherwise
-     */
-    public boolean isEmpty() {
-    	return nodeCount == 0;
-    }
+	private int nodeCount; // number of nodes in AVL tree
+	public Node root; // the root of the tree
+	private int height; // the height of the tree
 
-    /**
-     * Return the height of the AVLTree
-     * 
-     * @return int height
-     */
-    public int height() {
-    	return height;
-    }
-    
-    /**
-     * Returns the number of elements in the AVL tree
-     * 
-     * @return integer, the number of elements in the AVL tree
-     */
-    public int size() {
-    	return nodeCount;
-    }
+	// constructor - initialize fields
+	public AVLTree() {
+		this.nodeCount = 0;
+		this.root = null;
+		this.height = 0;
+	}
 
-    /**
-     * Returns the root node of the AVL tree
-     * 
-     * @return Node root
-     */
-    public Node root() {
-    	return root;
-    }
+	/**
+	 * Returns true when AVL tree is empty, false otherwise
+	 * 
+	 * @return boolean true, if empty, false otherwise
+	 */
+	public boolean isEmpty() {
+		return nodeCount == 0;
+	}
 
-    /**
-     * Insert a new Node into the AVLTree with value val
-     * Call recursive helper function insertHelper with (root, val)
-     * Note: pass root node to helper
-     * @return integer, the number of elements in the AVL tree
-     */
+	/**
+	 * Return the height of the AVLTree
+	 * 
+	 * @return int height
+	 */
+	public int height() {
+		return height;
+	}
 
-    public void insert(int val) {
-        if (root == null) {
-    		// create a new node of AVL tree if the tree is empty
-    		root = new Node(val);	
-            nodeCount = 1; 
-            this.height = 0;
-    	}
-        else {
-            insertHelper(root, val, 1);
-        }
-    }
-    
-    /**
-     * Recursive helper function for insert
-     * Creates new Node object with value val and iterates through the tree to find the correct position to insert
-     * 
-     * @param node, the node tha val will be compared against to determine position for insert
-     * @param val, the integer value to insert (new Node with value val) into the AVL tree
-     * @return AVLTree, the root node of the AVL tree
-     */
-    
-    public Node insertHelper(Node node, int val, int height) {
+	/**
+	 * Returns the number of elements in the AVL tree
+	 * 
+	 * @return integer, the number of elements in the AVL tree
+	 */
+	public int size() {
+		return nodeCount;
+	}
 
-    	if (val < node.val) {
-    		if (node.left == null) {
-    			node.left = new Node(val);
-                nodeCount++;
-                if (this.height < height) {this.height = height;}
-    			return node.left;
-    		}
-    		return insertHelper(node.left, val, height+1);
-    	}
-    	
-    	if (val > node.val) {
-    		if (node.right == null) {
-    			node.right = new Node(val);
-                nodeCount++;
-                if (this.height < height) {this.height = height;}
-    			return node.right;
-    		}
-    		return insertHelper(node.right, val, height+1);
-    	}
-    	return root;
-    }
-    
-    /**
-     * Checks whether the AVL Tree is balanced or not
-     * @param root, the root of the AVL tree
-     * @return true/false, true if AVL tree is balanced
-     */
-    public boolean isBalanced(Node root)
-    {
-    	if (root == null)
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		int left_treeHeight = root.left.height;
-    		int right_treeHeight = root.right.height;
-    		
-    		if (Math.abs(left_treeHeight-right_treeHeight)<=1 && isBalanced(root.left) && isBalanced(root.right))
-    		{
-    			return true; // is balanced
-    		}
-    		return false;
-    	}
-    }
-    
-    
-    /**
-     * Removes a node from the AVL tree given its value. If the tree is
-     * unbalanced after removal, the balance function is called.
-     * 
-     * @param root, the root of the tree
-     * @param val, the value of the node that is going to be removed
-     * @return Node object, the removed node
-     */
-    
-    public Node remove(Node root, int val)
-    {
-    	if (root==null)  // if tree is empty
-    	{
-    		return root;
-    	}
-    	
-    	if (root.left==null && root.right==null)  // if tree has one node
-    	{
-    		root = null;
-    	}
- 
-    	else  // if tree has more than one node
-    	{
-    		if (val < root.val)
-    		{
-    			root.left = remove(root.left, val);
-    		}
-    		else if (val > root.val)
-    		{
-    			root.right = remove(root.right, val);
-    		}
-    		else // found node, delete
-    		{
-    			//no children
-    			if (root.left == null && root.right == null)
-    			{
-    				root = null; 
-    			}
-    			
-    			//two children; replacement will be its right node
-    			else if (root.left != null && root.right!=null)
-    			{
-    				Node replacement = root.right;
-    				root.val = replacement.val;
-    				
-    				// remove the replacements old position Node
-    				root.right = remove(root.right, replacement.val);
-    				
-    			}
-    			// node has one child
-    			else
-    			{
-    				if (root.left == null)
-    				{
-    					root = root.right;
-    				}
-    				else
-    				{
-    					root = root.left;
-    				}
-    			}
-    		}
-    	}
-		root.height = 1 + Math.max(root.left.height, root.right.height);
-    	
-		if (isBalanced(root())==false)
+	/**
+	 * Returns the root node of the AVL tree
+	 * 
+	 * @return Node root
+	 */
+	public Node root() {
+		return root;
+	}
+
+	/**
+	 * Insert a new Node into the AVLTree with value val Call recursive helper
+	 * function insertHelper with (root, val) Note: pass root node to helper
+	 * 
+	 * @return integer, the number of elements in the AVL tree
+	 */
+
+	public void insert(int val) {
+		if (root == null) {
+			// create a new node of AVL tree if the tree is empty
+			root = new Node(val);
+			nodeCount = 1;
+			this.height = 0;
+		} else {
+			insertHelper(root, val, 1);
+		}
+	}
+
+	/**
+	 * Recursive helper function for insert Creates new Node object with value val
+	 * and iterates through the tree to find the correct position to insert
+	 * 
+	 * @param node, the node tha val will be compared against to determine position
+	 *              for insert
+	 * @param val,  the integer value to insert (new Node with value val) into the
+	 *              AVL tree
+	 * @return AVLTree, the root node of the AVL tree
+	 */
+
+	public Node insertHelper(Node node, int val, int height) {
+		Node newNode = null;
+
+		if (val < node.val) {
+			if (node.left == null) {
+				newNode = new Node(val);
+				newNode.height = height;
+				node.left = newNode;
+				nodeCount++;
+				return node.left;
+			}
+			return insertHelper(node.left, val, height + 1);
+		}
+
+		if (val > node.val) {
+			if (node.right == null) {
+				newNode = new Node(val);
+				newNode.height = height;
+				node.right = newNode;
+				nodeCount++;
+				return node.right;
+			}
+			return insertHelper(node.right, val, height + 1);
+		}
+		return root;
+	}
+
+	/**
+	 * Checks whether the AVL Tree is balanced or not
+	 * 
+	 * @param root, the root of the AVL tree
+	 * @return true/false, true if AVL tree is balanced
+	 */
+	public boolean isBalanced(Node root) {
+		if (root == null) {
+			return true;
+		} else {
+			int left_treeHeight = root.left.height;
+			int right_treeHeight = root.right.height;
+
+			if (Math.abs(left_treeHeight - right_treeHeight) <= 1 && isBalanced(root.left) && isBalanced(root.right)) {
+				return true; // is balanced
+			}
+			return false;
+		}
+	}
+
+	/**
+	 * Removes a node from the AVL tree given its value. If the tree is unbalanced
+	 * after removal, the balance function is called.
+	 * 
+	 * @param root, the root of the tree
+	 * @param val,  the value of the node that is going to be removed
+	 * @return Node object, the removed node
+	 */
+
+	public Node remove(Node root, int val) {
+		if (root == null) // if tree is empty
 		{
-			// balance() 
+			return root;
+		}
+
+		if (root.left == null && root.right == null) // if tree has one node
+		{
+			root = null;
+		}
+
+		else // if tree has more than one node
+		{
+			if (val < root.val) {
+				root.left = remove(root.left, val);
+			} else if (val > root.val) {
+				root.right = remove(root.right, val);
+			} else // found node, delete
+			{
+				// no children
+				if (root.left == null && root.right == null) {
+					root = null;
+				}
+
+				// two children; replacement will be its right node
+				else if (root.left != null && root.right != null) {
+					Node replacement = root.right;
+					root.val = replacement.val;
+
+					// remove the replacements old position Node
+					root.right = remove(root.right, replacement.val);
+
+				}
+				// node has one child
+				else {
+					if (root.left == null) {
+						root = root.right;
+					} else {
+						root = root.left;
+					}
+				}
+			}
+		}
+		root.height = 1 + Math.max(root.left.height, root.right.height);
+
+		if (isBalanced(root()) == false) {
+			// balance()
 			// call the balance function to balance the tree
 		}
-		
-		return root;
-    }
-    
-    
-    /**
-     * String representation of the AVL tree
-     * 
-     * @return String, the string generated for the representation
-     */
-    public String toString() {
-    	String str = "";
-    	Node tmp = root;
-    	
-    	// check if AVL tree is empty
-    	if (tmp == null) {
-    		str += "AVL tree is empty.";
-    		return str;
-    	}
-    	
-    	// return string representation of the tree  	
-		return str + toStringHelper(tmp, str);
-    }
 
-    /**
-     * Helper method to generate string representation of tree
-     * 
-     * @param tmp, the Node to display
-     * @param str, the string representation to add into
-     * @return String, the string generated for the representation
-     */
+		return root;
+	}
+
+	/**
+	 * String representation of the AVL tree
+	 * 
+	 * @return String, the string generated for the representation
+	 */
+	public String toString() {
+		String str = "";
+		Node tmp = root;
+
+		// check if AVL tree is empty
+		if (tmp == null) {
+			str += "AVL tree is empty.";
+			return str;
+		}
+		str += "AVL Tree:\n";
+
+		// return string representation of the tree
+		return str + toStringHelper(tmp, str);
+	}
+
+	/**
+	 * Helper method to generate string representation of tree
+	 * 
+	 * @param tmp, the Node to display
+	 * @param str, the string representation to add into
+	 * @return String, the string generated for the representation
+	 */
 	private String toStringHelper(Node tmp, String str) {
-    	if (tmp == null)
-    		return null;
-    	
-    	// add spaces based on node height
-    	int spaces = (tmp.height * 3);
-    	while (spaces-- != 0) {
-    		System.out.print(" ");
-    	}
-    	
+		if (tmp == null)
+			return null;
+
+		// add spaces based on node height
+		int spaces = (tmp.height * 3);
+		while (spaces-- != 0) {
+			System.out.print(" ");
+		}
+
 		System.out.println(tmp.val + "[Height=" + tmp.height + "]");
-		
+
 		// display left subtree
 		if (tmp.left != null)
 			str += toStringHelper(tmp.left, str);
-		
+
 		// display right subtree
 		if (tmp.right != null)
 			str += toStringHelper(tmp.right, str);
-		
+
 		return str;
 	}
 }
--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@
  */
 
 public class Test {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         String result;
         // Constructor test
         AVLTree tree = new AVLTree();
@@ -16,8 +16,16 @@ public class Test {
         result = tree.isEmpty() ? "PASSED" : "FAILED" ;
         System.out.println("Init isEmpty() test: " + result);
         // Insert test 1
-        // tree.insert()
-        // result = !tree.isEmpty() ? "PASSED" : "FAILED" ;
-        // System.out.println("Insert() test 1: " + result);
+         tree.insert(20);
+         for (int i = 0; i < 10; i++) {
+        	 tree.insert(i);
+         }
+         for (int i = 11; i < 20; i++) {
+        	 tree.insert(i);
+         }
+
+         System.out.println(tree);
+         result = !tree.isEmpty() ? "PASSED" : "FAILED" ;
+         System.out.println("Insert() test 1: " + result);
     }
 }
